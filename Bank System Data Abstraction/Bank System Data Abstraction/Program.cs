e31b7bf6e90b4a69128a0976706f8c90b2ce7eb6
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-27 11:45:22 -0600

    display inheritance at work

--- Bank System Data Abstraction/Bank System Data Abstraction/Program.cs
+++ Bank System Data Abstraction/Bank System Data Abstraction/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to close the program.");
+            Bank bank = new("UNC Bank");
+            Member john = new("John");
+            bank.AddMember(john);
+            john.AddAccount(new CheckingAccount("John's Checking Account", 10.00));
+            john.AddAccount(new SavingsAccount("John's Savings Account", 120.00));
+            bank.UpdateBankBalance();
+            john.PrintMemberInfo();
+
+            Console.WriteLine("\nPress any key to close the program.");
             Console.ReadKey();
         }
     }
