93726ac45508fee30ad6c9b7344b99de4e32ab77
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-10-27 10:56:43 -0600

    remove old account type attribute

--- Bank System Data Abstraction/Bank System Data Abstraction/Account.cs
+++ Bank System Data Abstraction/Bank System Data Abstraction/Account.cs
@@ -8,17 +8,14 @@ namespace Bank_System_Data_Abstraction
 {
     internal class Account
     {
-        public Account(string accountName, string accountType, double accountBalance)
+        public Account(string accountName, double accountBalance)
         {
             AccountName = accountName;
-            AccountType = accountType;
             AccountBalance = accountBalance;
             TransactionCount = 0;
         }
 
         public string AccountName { get; set; }
-
-        private string AccountType;
 
         private double AccountBalance;
 
@@ -28,12 +25,6 @@ namespace Bank_System_Data_Abstraction
         {
             return AccountBalance;
         }
-
-        public string GetAccountType()
-        {
-            return AccountType;
-        }
-
         public void AddFunds(double amount)
         {
             AccountBalance += amount;
@@ -48,7 +39,6 @@ namespace Bank_System_Data_Abstraction
         public double CloseAccount()
         {
             AccountName += "- DELETED";
-            AccountType += "- DELETED";
             return AccountBalance;
         }
     }
