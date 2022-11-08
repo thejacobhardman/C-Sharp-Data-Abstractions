2a19f8e4e90f34bb592f0c0a266304115bbb6070
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-11-08 11:51:13 -0700

    implement other team type creation

--- Basketball Game Data Abstraction/Basketball Game Data Abstraction/Program.cs
+++ Basketball Game Data Abstraction/Basketball Game Data Abstraction/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Ball ball = new Ball(new Location(2, 1));
+            // Basketball Methods
+            Ball ball = new(new Location(2, 1));
             Court basketBallCourt = new(ball);
 
             Basket northBasket = new Basket("North Basket", new Location(0, 4));
@@ -13,27 +14,69 @@
             basketBallCourt.AddBasket(northBasket);
             basketBallCourt.AddBasket(southBasket);
 
-            Team team1 = new("Panthers");
-            Team team2 = new("Celtics");
+            // Soccer Teams
+            Team soccerTeam1 = new("Patriots");
+            Team soccerTeam2 = new("Broncos");
 
-            Player jimmy = new("Jimmy", 0, new Location(0, 0));
-            Player tommy = new("Tommy", 1, new Location(3, 5));
-            Player bobby = new("Bobby", 2, new Location(4, 4));
+            Player soccerPlayerJimmy = new("Jimmy", 0, new Location(0, 0));
+            Player soccerPlayerTommy = new("Tommy", 1, new Location(3, 5));
+            Player soccerPlayerBobby = new("Bobby", 2, new Location(4, 4));
 
-            team1.AddPlayer(jimmy);
-            team1.AddPlayer(tommy);
-            team1.AddPlayer(bobby);
+            soccerTeam1.AddPlayer(soccerPlayerJimmy);
+            soccerTeam1.AddPlayer(soccerPlayerTommy);
+            soccerTeam1.AddPlayer(soccerPlayerBobby);
 
-            Player larry = new("Larry", 0, new Location(2, 4));
-            Player harry = new("Harry", 1, new Location(7, 5));
-            Player barry = new("Barry", 2, new Location(7, 3));
+            Player soccerPlayerLarry = new("Larry", 0, new Location(2, 4));
+            Player soccerPlayerHarry = new("Harry", 1, new Location(7, 5));
+            Player soccerPlayerBarry = new("Barry", 2, new Location(7, 3));
 
-            team2.AddPlayer(larry);
-            team2.AddPlayer(harry);
-            team2.AddPlayer(barry);
+            soccerTeam2.AddPlayer(soccerPlayerLarry);
+            soccerTeam2.AddPlayer(soccerPlayerHarry);
+            soccerTeam2.AddPlayer(soccerPlayerBarry);
 
-            basketBallCourt.AddTeam(team1);
-            basketBallCourt.AddTeam(team2);
+            // Football Teams
+            Team footballTeam1 = new("Patriots");
+            Team footballTeam2 = new("Broncos");
+
+            Player footballPlayerJimmy = new("Jimmy", 0, new Location(0, 0));
+            Player footballPlayerTommy = new("Tommy", 1, new Location(3, 5));
+            Player footballPlayerBobby = new("Bobby", 2, new Location(4, 4));
+
+            footballTeam1.AddPlayer(footballPlayerJimmy);
+            footballTeam1.AddPlayer(footballPlayerTommy);
+            footballTeam1.AddPlayer(footballPlayerBobby);
+
+            Player footballPlayerLarry = new("Larry", 0, new Location(2, 4));
+            Player footballPlayerHarry = new("Harry", 1, new Location(7, 5));
+            Player footballPlayerBarry = new("Barry", 2, new Location(7, 3));
+
+            footballTeam2.AddPlayer(footballPlayerLarry);
+            footballTeam2.AddPlayer(footballPlayerHarry);
+            footballTeam2.AddPlayer(footballPlayerBarry);
+
+            // Basketball Teams
+            Team basketballTeam1 = new("Panthers");
+            Team basketballTeam2 = new("Celtics");
+
+            Player basketballPlayerJimmy = new("Jimmy", 0, new Location(0, 0));
+            Player basketballPlayerTommy = new("Tommy", 1, new Location(3, 5));
+            Player basketballPlayerBobby = new("Bobby", 2, new Location(4, 4));
+
+            basketballTeam1.AddPlayer(basketballPlayerJimmy);
+            basketballTeam1.AddPlayer(basketballPlayerTommy);
+            basketballTeam1.AddPlayer(basketballPlayerBobby);
+
+            Player basketballPlayerLarry = new("Larry", 0, new Location(2, 4));
+            Player basketballPlayerHarry = new("Harry", 1, new Location(7, 5));
+            Player basketballPlayerBarry = new("Barry", 2, new Location(7, 3));
+
+            basketballTeam2.AddPlayer(basketballPlayerLarry);
+            basketballTeam2.AddPlayer(basketballPlayerHarry);
+            basketballTeam2.AddPlayer(basketballPlayerBarry);
+
+            // Basketball Methods
+            basketBallCourt.AddTeam(basketballTeam1);
+            basketBallCourt.AddTeam(basketballTeam2);
 
             basketBallCourt.UpdateCourt();
             basketBallCourt.PrintPlayers();
