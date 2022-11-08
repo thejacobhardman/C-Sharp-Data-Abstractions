b39ad73b65712c5a109a707d597a770703c81f94
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-11-08 11:28:07 -0700

    add employee class

---
+++ Basketball Game Data Abstraction/Basketball Game Data Abstraction/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball_Game_Data_Abstraction
+{
+    internal class Employee
+    {
+        public Employee (string name, int number, Team? team, Location location)
+        {
+            Name = name;
+            Number = number;
+            Location = location;
+        }
+
+        public string Name { get; set; }
+
+        public int Number { get; set; }
+
+        public Team? Team { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
